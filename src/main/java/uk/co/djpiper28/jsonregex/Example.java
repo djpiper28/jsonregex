acf4b5eec3ec61026e08a9e70beab8d26c424e4f
Danny Piper <danny.piper@bjss.com>
2023-10-18 17:10:16 +0100

    feat: updated examples

--- src/main/java/uk/co/djpiper28/jsonregex/Example.java
+++ src/main/java/uk/co/djpiper28/jsonregex/Example.java
@@ -1,14 +1,24 @@
 package uk.co.djpiper28.jsonregex;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         final String regex = new JsonRegexGenerator(
                 new JsonObject()
-                        .addNode(new JsonNode("code", new JsonString("\\d{6}"), true))
-                        .addNode(new JsonNode("test", new JsonString("\\d{6}"), true))
+                        .addNode(new JsonNode("line1", new JsonString(JsonString.DEFAULT_PATTERN)))
+                        .addNode(new JsonNode("line2", new JsonString(JsonString.DEFAULT_PATTERN), true))
+                        .addNode(new JsonNode("line3", new JsonString(JsonString.DEFAULT_PATTERN), true))
+                        .addNode(new JsonNode("county", new JsonString(JsonString.DEFAULT_PATTERN), true))
+                        .addNode(new JsonNode("country", new JsonString(JsonString.DEFAULT_PATTERN)))
+                        .addNode(new JsonNode("townCity", new JsonString(JsonString.DEFAULT_PATTERN)))
+                        .addNode(new JsonNode("postcode", new JsonString(JsonString.DEFAULT_PATTERN)))
         ).getRegex();
 
-        System.out.println(JsonRegexGenerator.escapeString(regex));
-        System.out.println(regex);
+        final PrintWriter pw = new PrintWriter(new File("output.txt"));
+        pw.println(JsonRegexGenerator.escapeString(regex));
+        pw.close();
     }
 }
